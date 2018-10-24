8c7c1976134f76cd4378f7ef4d2738b8fd6312ef
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 20:32:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
+
+  def create
+    resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+      req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
+      req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
+      req.params[''] = ''
+      req.params[''] = ''
+      req.params[''] = ''
+    end
+  end
 end
