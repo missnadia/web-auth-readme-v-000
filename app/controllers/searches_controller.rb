870073fde6255a105add5337ca8f7b21d20746e8
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 20:37:43 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,6 @@ class SearchesController < ApplicationController
     client_secret = ENV['FOURSQUARE_SECRET']
 
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['oauth_token'] = sessions[:token]
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['v'] = '20160201'
